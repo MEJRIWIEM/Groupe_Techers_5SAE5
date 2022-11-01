5c79e6cfe5568415db5867b68e4a547256308403
unknown <sabri.rakrouki@esprit.tn>
2022-11-01 20:38:07 +0100

    test facture working

--- src/test/java/tn/esprit/rh/achat/FactureServiceImpTest.java
+++ src/test/java/tn/esprit/rh/achat/FactureServiceImpTest.java
@@ -1,9 +1,12 @@
 package tn.esprit.rh.achat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -26,12 +29,12 @@ import static org.mockito.Mockito.times;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
  class FactureServiceImpTest {
-	@Autowired
+	@InjectMocks
 	private FactureServiceImpl factureServiceImpl;
 	@MockBean
 	private FactureRepository factureRepository;
-
 	@Test
 	public void retrieveAllFactures() {
 		 when(factureRepository.findAll()).thenReturn(Stream.of(new Facture(),new Facture(),new Facture()).collect(Collectors.toList())) ; 
